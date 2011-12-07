bec212f31b84318593e03f9197458c2a15a8a8cb
Sam Wilson <tecywiz121@hotmail.com>
2011-12-07 10:44:33 -0500

    Adding some signature tests

--- arkli/test/signature.py
+++ arkli/test/signature.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import unittest
-from arkli.signature import quote_rfc3986
+from arkli.signature import quote_rfc3986, create_signature
 
 class TestQuoteFunction(unittest.TestCase):
     def test_none(self):
@@ -26,6 +26,37 @@ class TestQuoteFunction(unittest.TestCase):
         test = 'at@here'
         result = quote_rfc3986(test)
         self.assertEqual(result, 'at%40here')
+
+class TestSignatureFunction(unittest.TestCase):
+    def test_blank(self):
+        '''
+        Tests signing a blank request
+        '''
+        result = create_signature('', '', '', '', 1, 'nonce');
+        result = dict(result)
+        self.assertIn('arkli_key', result)
+        self.assertIn('arkli_nonce', result)
+        self.assertIn('arkli_timestamp', result)
+        self.assertIn('arkli_user', result)
+        self.assertIn('arkli_signature', result)
+        
+        self.assertEqual(result['arkli_signature'],
+                            'NJFLQn3cpfZM9gF+ESFKGFInFqg=')
+    
+    def test_no_email(self):
+        '''
+        Tests signing a standard request
+        '''
+        result = create_signature('key', 'secret', 'test-user', '', 1, 'nonce');
+        result = dict(result)
+        self.assertIn('arkli_key', result)
+        self.assertIn('arkli_nonce', result)
+        self.assertIn('arkli_timestamp', result)
+        self.assertIn('arkli_user', result)
+        self.assertIn('arkli_signature', result)
+        
+        self.assertEqual(result['arkli_signature'],
+                            'SoPo6WtIERgfKTo9I/Z1wDQuNSU=')
 
 if __name__ == '__main__':
     unittest.main()
